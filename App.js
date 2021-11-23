bedc3c65fd68f2ba07a891c8ab70d81b6dbcac88
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-11-23 22:49:00 +0100

    wip google auth

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import tw from "tailwind-rn";
 import StackNavigator from "./StackNavigator";
+import { AuthProvider } from "./hooks/useAuth";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <AuthProvider>
       < StackNavigator/>
+      </AuthProvider>
     </NavigationContainer>
   );
 }
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import useAuth from '../hooks/useAuth';
+
 
 export default function LoginScreen() {
+    const { user} = useAuth();
+    console.log(user)
     return (
         <View>
             <Text>Login Screen</Text>
