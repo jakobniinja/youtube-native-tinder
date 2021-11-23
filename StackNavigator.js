0ff460c91fe71a29339e1023a9393fc3cecdf724
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-11-23 16:58:37 +0100

    impl stack.navigation

---
+++ StackNavigator.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import ChatScreen from "./screens/ChatScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function StackNavigator() {
+  const user =true;
+
+  return (
+    <Stack.Navigator>
+      {user ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Chat" component={ChatScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      )}
+    </Stack.Navigator>
+  );
+}
