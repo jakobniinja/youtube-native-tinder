d4ffe076b958f94b96357fbddb95fc21f73aef13
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-11-23 16:58:19 +0100

    impl chat screen

---
+++ screens/ChatScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function ChatScreen() {
+    return (
+        <View>
+            <Text>
+                I'm the ChatScren
+            </Text>
+        </View>
+    )
+}
