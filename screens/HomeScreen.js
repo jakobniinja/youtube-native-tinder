69482964b8bb0a0309ddd899260402b60d41f968
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-11-27 17:56:30 +0100

    impl logout

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,12 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text, Button } from "react-native";
+import useAuth from "../hooks/useAuth"
 
 export default function HomeScreen() {
   const navigation = useNavigation();
+  const {logout} = useAuth();
+
   return (
     <View>
       <Text>homescren</Text>
@@ -11,6 +14,11 @@ export default function HomeScreen() {
         title="Go to chat"
         color="#8a2be2"
         onPress={() => navigation.navigate("Chat")}
+      ></Button>
+      <Button
+        title="logout"
+        color="#8a2be2"
+        onPress={logout}
       ></Button>
     </View>
   );
