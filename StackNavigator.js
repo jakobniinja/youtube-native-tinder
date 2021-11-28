2471cf9a9a8ee65254d157cc48554d768afd2ebb
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-11-28 22:17:53 +0100

    temporary bypass login screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -7,16 +7,25 @@ import useAuth from "./hooks/useAuth";
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
-  const {user} = useAuth()
+  // const { user } = useAuth();
+  const user = true
   return (
-    <Stack.Navigator>
+    <Stack.Navigator 
+    screenOptions={{
+      headerShown: false,
+    }}
+    >
       {user ? (
         <>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Chat" component={ChatScreen} />
         </>
       ) : (
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          // options={{ headerShown: false }}
+        />
       )}
     </Stack.Navigator>
   );
