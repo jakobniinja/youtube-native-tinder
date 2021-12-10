f23503225ff925e30e662c367178c33d622c12d1
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-10 13:31:16 +0100

    impl messagescreen

--- StackNavigator.js
+++ StackNavigator.js
@@ -21,6 +21,7 @@ export default function StackNavigator() {
           <Stack.Group>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Chat" component={ChatScreen} />
+            <Stack.Screen name="Message" component={MessageScreen} />
           </Stack.Group>
           <Stack.Group screenOptions={{ presentation: "modal" }}>
             <Stack.Screen name="Modal" component={ModalScreen} />
