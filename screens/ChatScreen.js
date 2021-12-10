2fe95adfe9c70970521fbccb9a2a2616daf52bf8
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-10 13:30:44 +0100

    impl chatscreen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import  Header  from '../component/Header'
 
 export default function ChatScreen() {
     return (
-        <View>
-            <Text>Chat</Text>
-        </View>
-    )
-}
+        <SafeAreaView>
+            <Header title="Chat" />
+        </SafeAreaView>
+    );
+};
