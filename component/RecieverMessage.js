6fcd16ea85984065f75bcfdb44b7467ed52830fa
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-11 18:43:30 +0100

    impl receiver message

---
+++ component/RecieverMessage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function ReceiverMessage({message}) {
+    return (
+        <View>
+            <Text>Reciver message</Text>
+        </View>
+    )
+}
