7cad8207560b98fb7683a1d99780391db5212708
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-11 18:43:56 +0100

    message screen -wip

---
+++ screens/MessageScreen.js
@@ -0,0 +1,79 @@
+import { useRoute } from "@react-navigation/native";
+import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+ FlatList,
+  Keyboard,
+} from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+
+
+import  SenderMessage from "../component/SenderMessage"
+import   RecieverMessage from "../component/RecieverMessage"
+import tw from "tailwind-rn";
+import Header from "../component/Header";
+import useAuth from "../hooks/useAuth";
+
+const MessageScreen = ({ matchDetails }) => {
+  const user = useAuth();
+  const { params } = useRoute();
+  const { matchDetails } = params;
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([])
+
+  const sendMessage = () => {};
+
+  return (
+    <SafeAreaView style={tw("flex-1")}>
+      <Header
+        title={getMatchUserInfo(matchDetails.users, user.uid)}
+        callEnabled
+      />
+
+      <KeyboardAvoidingView
+        behavior={(Platform.OS = "ios" ? "padding" : "height")}
+        style={tw("flex-1")}
+        keyboardVerticalOffset={10}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <FlatList 
+          data={messages} 
+          style={tw("pl-4")}
+          keyExtractor={item = item.id}
+          renderItem={({item : message}) => 
+           message.userId === user.uid? (
+            <SenderMessage  key={message.id} message={message} />
+           ):(
+             <RecieverMessage key={message.id} message={message}  />
+           ) 
+          }
+         >  </FlatList > 
+        </TouchableWithoutFeedback>
+
+        {/* <Text>message screen</Text> */}
+        <View
+          style={tw(
+            "flex-row justify-between bg-white items-center border-t border-gray-200 px-5 py-2"
+          )}
+        >
+          <TextInput
+            style={tw("h-10 text-lg")}
+            placeholder="Send Message..."
+            onChangeText={setInput}
+            onSubmitEditing={sendMessage}
+            value={input}
+          ></TextInput>
+          <Button title="Send" color="#ff5864" onPress={sendMessage}></Button>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export default MessageScreen;
