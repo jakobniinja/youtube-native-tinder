578fb4e8f5f8ec3ab1bc8baadac59c5b94e3fd5f
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-12 13:23:55 +0100

    update receiver message

--- component/RecieverMessage.js
+++ component/RecieverMessage.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
+import tw from "tailwind-rn"
 
 export default function ReceiverMessage({message}) {
     return (
-        <View>
-            <Text>Reciver message</Text>
+        <View style={[tw("bg-red-400 rounded-lg rounded-tl-non px-5 py-3 my-2 ml-14"), {alignSelf: "flex-start"} ]} >
+            <Image style={tw("h-12 w-12 rounded-full absolute top-0 left-14")}
+            source={{uri: message.photoURL}}
+            />
+            <Text style={tw("text-white")} >
+                {messages.message}
+            </Text>
         </View>
     )
 }
